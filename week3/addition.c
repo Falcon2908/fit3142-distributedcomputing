62fdf2ccdb5cd6de6b7db843de8d13f20f8fb3ab
AWiyono <arvinwiyono@gmail.com>
2016-08-08 23:56:02 +1000

    learn how to import functions

---
+++ week3/addition.c
@@ -0,0 +1,3 @@
+int add_two_numbers(int x, int y){
+  return x + y;
+}
--- week3/hello.c
+++ week3/hello.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
+#include "addition.h"
 int main(){
   printf("Hello World!\n");
   int* integer_pointer;
   int i = 0;
   integer_pointer = &i;
   printf("memory location %p, value: %d\n", integer_pointer, *integer_pointer);
+  int result = add_two_numbers(2,3);
+  printf("result of addition: %d\n", result);
   return 0;
 }
