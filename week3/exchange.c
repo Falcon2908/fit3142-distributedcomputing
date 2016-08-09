690fc1b668a72bfab2002a44dfe35ee5d8d4f71d
AWiyono <arvinwiyono@gmail.com>
2016-08-09 10:14:51 +1000

    fixed bug in the exchange() function

--- week3/exchange.c
+++ week3/exchange.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-// void exchange(int* a, int* b);
-void exchange(int a, int b);
+void exchange(int* a, int* b);
+// void exchange(int a, int b);
 
 int main(int argc, char* argv[]){
   int a, b;
@@ -9,26 +9,26 @@ int main(int argc, char* argv[]){
   b = 7;
 
   printf("From main a = %d, b = %d\n", a, b);
-  exchange(a, b);
+  exchange(&a, &b);
   printf("Back in main: ");
   printf("a = %d, b = %d\n", a, b);
   return 0;
 }
 
-// void exchange(int* a, int* b){
-//   int* temp;
+void exchange(int* a, int* b){
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+  printf("From function exchange: ");
+  printf("a = %d, b = %d\n", *a, *b);
+}
+//
+// void exchange(int a, int b){
+//   int temp;
 //   temp = a;
 //   a = b;
 //   b = temp;
 //   printf("From function exchange: ");
-//   printf("a = %d, b = %d\n", *a, *b);
+//   printf("a = %d, b = %d\n", a, b);
 // }
-
-void exchange(int a, int b){
-  int temp;
-  temp = a;
-  a = b;
-  b = temp;
-  printf("From function exchange: ");
-  printf("a = %d, b = %d\n", a, b);
-}
