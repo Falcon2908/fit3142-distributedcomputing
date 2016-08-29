6d7350f756d93a51bd91fff8cb6203562ca27170
AWiyono <arvinwiyono@gmail.com>
2016-08-30 00:14:49 +1000

    implemented server side (tested with telnet 0.0.0.0 5000)

--- week6/server.c
+++ week6/server.c
@@ -9,12 +9,19 @@
 #include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
 #define PORT "5000"
+#define BACKLOG 10
+#define BUFFSIZE 1024
 
 int main(int argc, char* argv[]){
 
   int status;
   struct addrinfo hints, *servinfo;
+
+  fprintf(stdout, "fit3142 - server application\n");
 
   // Perform getaddrinfo()
   memset(&hints, 0, sizeof(hints));
@@ -23,11 +30,68 @@ int main(int argc, char* argv[]){
   hints.ai_flags = AI_PASSIVE;
 
   if((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) == -1){
-    fprintf(stderr, "error getaddrinfo(): %s\n", gai_strerror(status));
+    perror("error: getaddrinfo()\n");
     exit(1);
   }
 
   // servinfo now points to a linked list of 1 or more struct addrinfos
+  struct addrinfo *p;
+  int socketfd;
+  for(p = servinfo; p != NULL; p = p->ai_next){
+    // Get socket file descriptor
+    if((socketfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+      perror("error: socket()\n");
+      continue;
+    }
+    else{
+      fprintf(stdout, "socket success!\n");
+    }
+    // Perform binding
+    if(bind(socketfd, p->ai_addr, p->ai_addrlen) == -1){
+      close(socketfd);
+      perror("error: bind()\n");
+      continue;
+    }
+    else{
+      fprintf(stdout, "binding success!\n");
+    }
+    break;
+  }
+  freeaddrinfo(servinfo); // Done with the structure
 
+  // If there is no socket that can be bound
+  if(p == NULL){
+    perror("error: server failed to bind\n");
+    exit(1);
+  }
+
+  // BACKLOG is the number of connections allowed on the incoming queue
+  if(listen(socketfd, BACKLOG) == -1){
+    perror("error: listen()\n");
+    exit(1);
+  }
+  fprintf(stdout, "server: waiting for connections...\n");
+
+  struct sockaddr_storage client_address;
+  socklen_t addr_size = sizeof(client_address);
+  int new_fd = accept(socketfd, (struct sockaddr *) &client_address, &addr_size);
+
+  if(new_fd == -1){
+    perror("error: accept()");
+    exit(1);
+  }
+
+  char buffer[BUFFSIZE] = "";
+  // char s[INET6_ADDRSTRLEN];
+  // inet_ntop(client_address.ss_family, get_in_addr((struct sockaddr *) &client_address), s, sizeof(s));
+  // fprintf(stdout, "server: got connection from %s\n", s);
+  while(recv(new_fd, buffer, BUFFSIZE, 0)){
+    fprintf(stdout, "%s", buffer);
+    memset(buffer,0,strlen(buffer));
+  }
+  close(new_fd);
+  close(socketfd);
+
+  fprintf(stdout, "connection is closed!\n");
   return 0;
 }
