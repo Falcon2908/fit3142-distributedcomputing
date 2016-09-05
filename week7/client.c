a6e7cac2be846d93fc2bd3a8649df33eb2b429a2
AWiyono <arvinwiyono@gmail.com>
2016-09-05 14:34:00 +1000

    added week 7 folder

---
+++ week7/client.c
@@ -0,0 +1,87 @@
+// FIT3142 - Client that connects to a server with BSD socket
+// Author: Arvin Wiyono
+// Date: 29 August 2016 - Semester 2
+// Based on beej.us tutorial on Network programming
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "segment-lock.h"
+#define PORT "5000"
+#define BACKLOG 10
+#define BUFFSIZE 1024
+
+void *get_in_addr(struct sockaddr *sa){
+  if (sa->sa_family == AF_INET){
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char* argv[]){
+  int status;
+  struct addrinfo hints, *servinfo;
+
+  fprintf(stdout, "fit3142 - client application\n");
+  if(argc != 2){
+    fprintf(stdout, "usage: client hostname\n");
+    exit(1);
+  }
+
+  // Perform getaddrinfo()
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC; // look for both IPv4 and IPv6 address
+  hints.ai_socktype = SOCK_STREAM; // can be either SOCK_STREAM or SOCK_DGRAM
+
+  if((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) == -1){
+    perror("error in getaddrinfo()");
+    exit(1);
+  }
+
+  // servinfo now points to a linked list of 1 or more struct addrinfos
+  struct addrinfo *p;
+  int socketfd;
+  for(p = servinfo; p != NULL; p = p->ai_next){
+    // Get socket file descriptor
+    if((socketfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+      perror("error in socket()");
+      fprintf(stdout, "continue to the next addrinfo\n");
+      continue;
+    }
+    // Perform connect
+    if(connect(socketfd, p->ai_addr, p->ai_addrlen) == -1){
+      close(socketfd);
+      perror("error in connect()");
+      fprintf(stdout, "continue to the next addrinfo\n");
+      continue;
+    }
+    break;
+  }
+
+  // If there is no socket that can be bound
+  if(p == NULL){
+    perror("error: client failed to connect ");
+    exit(1);
+  }
+
+  char s[INET6_ADDRSTRLEN];
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *) p->ai_addr), s, sizeof(s));
+  fprintf(stdout, "client: connecting to %s\n", s);
+  freeaddrinfo(servinfo); // Done with the structure
+
+  // start getting input from user
+  char buffer[BUFFSIZE];
+  while(fgets(buffer, BUFFSIZE, stdin) != NULL ){
+      send(socketfd, buffer, strlen(buffer), 0);
+  }
+  close(socketfd);
+
+  fprintf(stdout, "connection is closed!\n");
+  return 0;
+}
---
+++ week7/server.c
@@ -0,0 +1,104 @@
+// FIT3142 - Server with BSD socket
+// Author: Arvin Wiyono
+// Date: 29 August 2016 - Semester 2
+// Based on beej.us tutorial on Network programming
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "segment-lock.h"
+#define PORT "5000"
+#define BACKLOG 10
+#define BUFFSIZE 1024
+
+void *get_in_addr(struct sockaddr *sa){
+  if (sa->sa_family == AF_INET){
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char* argv[]){
+  SEG_DATA * mydata = (SEG_DATA*) malloc(sizeof(SEG_DATA));
+  
+  fprintf(stdout, "%d\n", mydata->mylock);
+  int status;
+  struct addrinfo hints, *servinfo;
+
+  fprintf(stdout, "fit3142 - server application\n");
+
+  // Perform getaddrinfo()
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC; // look for both IPv4 and IPv6 address
+  hints.ai_socktype = SOCK_STREAM; // can be either SOCK_STREAM or SOCK_DGRAM
+  hints.ai_flags = AI_PASSIVE;
+
+  if((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) == -1){
+    perror("error in getaddrinfo()");
+    exit(1);
+  }
+
+  // servinfo now points to a linked list of 1 or more struct addrinfos
+  struct addrinfo *p;
+  int socketfd;
+  for(p = servinfo; p != NULL; p = p->ai_next){
+    // Get socket file descriptor
+    if((socketfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+      perror("error in socket()");
+      continue;
+    }
+
+    // Perform binding
+    if(bind(socketfd, p->ai_addr, p->ai_addrlen) == -1){
+      close(socketfd);
+      perror("error in bind()");
+      continue;
+    }
+    break;
+  }
+  freeaddrinfo(servinfo); // Done with the structure
+
+  // If there is no socket that can be bound
+  if(p == NULL){
+    perror("error: server failed to bind\n");
+    exit(1);
+  }
+
+  // BACKLOG is the number of connections allowed on the incoming queue
+  if(listen(socketfd, BACKLOG) == -1){
+    perror("error: listen()\n");
+    exit(1);
+  }
+
+  fprintf(stdout, "server: waiting for connections...\n");
+  struct sockaddr_storage client_address;
+  socklen_t addr_size = sizeof(client_address);
+  // accept() makes server wait until a client is attached
+  int new_fd = accept(socketfd, (struct sockaddr *) &client_address, &addr_size);
+
+  if(new_fd == -1){
+    perror("error: accept()");
+    exit(1);
+  }
+
+  char s[INET6_ADDRSTRLEN];
+  inet_ntop(client_address.ss_family, get_in_addr((struct sockaddr *) &client_address), s, sizeof(s));
+  fprintf(stdout, "server: got connection from %s\n", s);
+
+  char buffer[BUFFSIZE] = "";
+  while(recv(new_fd, buffer, BUFFSIZE, 0)){
+    fprintf(stdout, "%s", buffer);
+    memset(buffer,0,strlen(buffer));
+  }
+  close(new_fd);
+  close(socketfd);
+
+  fprintf(stdout, "connection is closed!\n");
+  return 0;
+}
